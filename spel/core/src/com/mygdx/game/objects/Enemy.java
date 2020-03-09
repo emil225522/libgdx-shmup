1963d2f30ef92238e2517fd19bdc039e88d9e348
emil225522 <ungothar@hotmail.com>
2020-03-09 21:44:35 +0100

    Added enemy

--- spel/core/src/com/mygdx/game/objects/Enemy.java
+++ spel/core/src/com/mygdx/game/objects/Enemy.java
@@ -1,26 +1,34 @@
 package com.mygdx.game.objects;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
-public class Bullet {
+public abstract class Enemy {
 	Texture texture;
 	Vector2 position;
+	Rectangle hitBox;
+	int life = 0;
+	boolean isDead = false;
 	
-	public Bullet(Vector2 position, Texture texture) {
+	public Enemy(Vector2 position, Texture texture, int life) {
 		this.texture = texture;
 		this.position = position;
+		this.life = life;
+		this.hitBox = new Rectangle(position.x,position.y,texture.getWidth(),texture.getHeight());
 	}
 	
 	public void update() {
-			position.add(5, 0);
-
+		
+		
+		if (life <= 0) {
+			isDead = true;
+		}
 	}
 	
 	public void draw(SpriteBatch spriteBatch) {
 		spriteBatch.draw(texture,position.x,position.y);
 	}
+
 }
